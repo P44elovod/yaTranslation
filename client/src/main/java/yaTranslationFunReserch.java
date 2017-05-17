d5df22b37a4dbb988818683b6d8b9c501e168d2b
P44elovod <eug.kovtun@gmail.com>
2017-05-18 00:25:34 +0300

    ++2

--- client/src/main/java/yaTranslationFunReserch.java
+++ client/src/main/java/yaTranslationFunReserch.java
@@ -6,43 +6,60 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 import java.util.StringTokenizer;
+import java.util.regex.Pattern;
 
 
 public class yaTranslationFunReserch {
 
-    public static void main(String[] args) throws Exception {
+    private static final Pattern TEXT_SPLIT_PATTERN = Pattern.compile("[!?]\\s*");
+
+    public static void main(String[] args) {
         Translate.setKey("trnsl.1.1.20170508T204041Z.28e80bf4ecfc6070.2c53fb29fe2e03715d5f594d7ea8ec10534faca7");
+        try {
 
+            writeToFile(Files.readAllLines(Paths.get("d:/in.txt")).stream()
+                    .map(s -> s.toString()
+                            .replaceAll(TEXT_SPLIT_PATTERN.toString(), ". ")
+                    )
+                    .map(yaTranslationFunReserch::translate)
+                    .reduce((s1, s2) -> {
+                        List<String> list = new ArrayList<>();
+                        list.add(s1.toString());
+                        list.add(s2.toString());
+                        return list;
 
-        Files.readAllLines(Paths.get("d:/in.txt")).stream()
-                .map(yaTranslationFunReserch::translate)
-                .forEach(yaTranslationFunReserch::writeToFile);
-
+                    }));
+        } catch (IOException e) {
+            System.out.println("что-то пошло не так :(");
+            e.printStackTrace();
+        }
 
     }
 
-    public static ArrayList<String> translate(String inLine) {
-        inLine = inLine.replaceAll("[!?]\\s*", ".");
+    public static List<String> translate(String inLine) {
+
         StringTokenizer tokenizer = new StringTokenizer(inLine, ".", true);
-        String line = "";
-        int g = 0;
-        ArrayList<String> lines = new ArrayList<>();
+        StringBuilder stringBuilder = new StringBuilder();
+        List<String> lines = new ArrayList<>();
+        int sentenceCounter = 0;
         while (tokenizer.hasMoreTokens()) {
-            line += tokenizer.nextToken();
-            g++;
-            if (g % 10 == 0) {
+            stringBuilder.append(tokenizer.nextToken());
+            sentenceCounter++;
+            if (sentenceCounter % 10 == 0) {
                 try {
                     for (int count = 0; count < 2; count++) {
-                        line = Translate.execute(line, Language.RUSSIAN, Language.ENGLISH);
-                        line = Translate.execute(line, Language.ENGLISH, Language.RUSSIAN);
+                        stringBuilder.replace(0, stringBuilder.length(), Translate.execute(stringBuilder.toString(), Language.RUSSIAN, Language.ENGLISH));
+                        stringBuilder.replace(0, stringBuilder.length(), Translate.execute(stringBuilder.toString(), Language.ENGLISH, Language.RUSSIAN));
 
                     }
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
-                lines.add(line);
-                line = "";
+                lines.add(stringBuilder.toString());
+                stringBuilder.delete(0, stringBuilder.length() + 1);
 
             }
 
@@ -53,7 +70,7 @@ public class yaTranslationFunReserch {
     }
 
 
-    public static void writeToFile(ArrayList<String> outLine) {
+    public static void writeToFile(Optional<List<String>> outLine) {
 
 
         try {
